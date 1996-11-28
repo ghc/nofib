fdeeed7ed15950d6b08a87b6bdbffe3aa5bb1c47
dnt <unknown>
1996-11-28 11:15:33 +0000

    [project @ 1996-11-28 11:15:17 by dnt] Haskell 1.3 has no n+k patterns, so this test is useless

--- UNUSED/nfib_n_plus_k/Main.hs
+++
@@ -1,9 +0,0 @@
---!!! the ultra-notorious "nfib 30" (w/ n+k pattern)
---
-module Main (main) where
-
-main = print (nfib 30)
-
-nfib 0       = 1
-nfib 1       = 1
-nfib (n + 2) = nfib n + nfib (n + 1) + 1
