b16916f6a638c756be09508edc92f869767f97bf
sof <unknown>
1997-05-18 01:34:13 +0000

    [project @ 1997-05-18 01:34:13 by sof] Support for invoking HC via RUNSTDTEST

--- mk/paths.mk
+++ mk/paths.mk
@@ -14,3 +14,14 @@ NOFIB_PROG = $(notdir $(shell pwd))$(_way)
 
 # Eventually, have the binary purged
 CLEAN_FILES += $(NOFIB_PROG)
+
+#
+# If tests are expected to end in compilation failure,
+# set the flag HC_MAY_FAIL to YES (before including boilerplate,mk !)
+#
+# Options to the runstdtest that we wrap around the `real' HC below
+# can be set through HC_RUNSTDTEST_OPTS
+#
+ifeq "$(HC_MAY_FAIL)" "YES"
+HC:=$(RUNSTDTEST) -x1 $(HC_RUNSTDTEST_OPTS) -- $(HC)
+endif
