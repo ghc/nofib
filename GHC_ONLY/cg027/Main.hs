76f1f3ed3f2f978353ef1a4ac2d98deba66d5b22
sof <unknown>
1997-06-05 23:02:05 +0000

    [project @ 1997-06-05 23:02:05 by sof] added Int Foo instance

--- GHC_ONLY/cg027/Main.hs
+++ GHC_ONLY/cg027/Main.hs
@@ -6,6 +6,8 @@ class (Num a, Integral a) => Foo a
 main = putStr (shows (f ((fromInteger 21)::Int)
 			((fromInteger 37))) "\n")
 
+instance Foo Int
+
 f :: Foo a => a -> a -> Integer
 
 f a b = toInteger (a + b)
