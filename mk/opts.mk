ab73c3ba4820e39f73fc40b3e47c2ec4f93b2e52
sof <unknown>
1998-10-21 16:47:47 +0000

    [project @ 1998-10-21 16:47:47 by sof] The default definition of RUNTEST_OPTS isn't as useful as it could be for NoFib, since it assumes that $(RUNTEST_OPTS) is being used in a pattern rule (i.e., $* is made use of.)
    Replace it with a more approp. NoFib definition, i.e., instead
    of $* use $(NOFIB_PROG).

--- mk/opts.mk
+++ mk/opts.mk
@@ -2,10 +2,18 @@
 #
 #			    nofib/mk/opts.mk
 #
-# 	$Id: opts.mk,v 1.2 1998/02/19 18:43:20 sof Exp $
+# 	$Id: opts.mk,v 1.3 1998/10/21 16:47:47 sof Exp $
 #
 #################################################################################
 
+# The default definition of RUNTEST_OPTS in $(TOP)/mk/opts.mk assume
+# that it is going to be used in a pattern rule. This not the case
+# for NoFib tests, so we define a custom version of RUNTEST_OPTS
+# that instead of $* uses $(NOFIB_PROG), so as to provide a way
+# to configure (and override) the options to run a particular test
+# with.
+RUNTEST_OPTS       = $(SRC_RUNTEST_OPTS) $(WAY$(_way)_RUNTEST_OPTS) \
+                     $($(NOFIB_PROG)_RUNTEST_OPTS) $(EXTRA_RUNTEST_OPTS)
 
 SRC_RUNTEST_OPTS += -ghc-timing +RTS -H20m -K10m -RTS
 
