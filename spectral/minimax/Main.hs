018ffe27cb625f52d48d3b5a066342a67a44d1fc
simonm <unknown>
1999-01-29 13:30:22 +0000

    [project @ 1999-01-29 13:30:22 by simonm] H98 update

--- spectral/minimax/Main.hs
+++ spectral/minimax/Main.hs
@@ -2,11 +2,11 @@ module Main where
 
 import Prog (prog)
 
---#ifdef PAR
---main input = prog input
---#else
---suspect:main ~((Str str):_) = [ReadChan stdin, AppendChan stdout (prog str)]
+-- #ifdef PAR
+-- main input = prog input
+-- #else
+-- suspect:main ~((Str str):_) = [ReadChan stdin, AppendChan stdout (prog str)]
 main = do
     _ <- getContents -- useless, but that's how it was written
     putStr (prog "")
---#endif
+-- #endif
