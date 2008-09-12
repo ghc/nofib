9f9f4dfa72219abb5878978c84c07164497fba3d
Ian Lynagh <igloo@earth.li>
2008-09-12 11:02:27 +0000

    Make USE_NEW_MKDEPEND_FLAGS=YES if MKDEPENDHS >= GHC 609

--- mk/boilerplate.mk
+++ mk/boilerplate.mk
@@ -31,7 +31,8 @@ WAYS=$(NoFibWays)
 SRC_HC_OPTS += $(NoFibHcOpts) -Rghc-timing
 
 HC	   = $(WithNofibHc)
-MKDEPENDHS = $(WithNofibHc)  # ToDo: wrong, if $(WithNofibHc) isn't GHC.
+MKDEPENDHS := $(WithNofibHc)  # ToDo: wrong, if $(WithNofibHc) isn't GHC.
+USE_NEW_MKDEPEND_FLAGS := $(shell if test `$(MKDEPENDHS) --numeric-version | sed -e "s/\./0/" -e "s/\..*//"` -ge 609; then echo YES; else echo NO; fi)
 
 include $(NOFIB_TOP)/mk/paths.mk
 include $(NOFIB_TOP)/mk/opts.mk
