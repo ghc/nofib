4599a07ce0ff9f355e14bf96db0dfe42c3aa8267
David Terei <davidterei@gmail.com>
2012-01-12 14:51:12 -0800

    Remove old files.

--- UNUSED/nfib/Main.hs
+++
@@ -1,8 +0,0 @@
---!!! the ultra-notorious "nfib 30"
---
-module Main (main) where
-
-main = print (nfib 30)
-
-nfib :: Int -> Int
-nfib n = if n <= 1 then 1 else nfib (n-1) + nfib (n-2) + 1
--- UNUSED/nfib/nfib.m
+++
@@ -1,2 +0,0 @@
-nfib n = 1, n<= 1
-       = 1 + nfib (n-1) + nfib (n-2), otherwise
