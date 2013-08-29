ghc/nofib

commit 957b959dbeb14c3b2d10998d67310e880c01a7dd

Follow changes in base

shootout/n-body/Main.hs
@@ -17,6 +17,7 @@ import Foreign.Marshal.Alloc
 import Data.IORef
 import Control.Monad
 import System.Environment
+import System.IO.Unsafe
 import Text.Printf
 
 main = do

commit 9701fb0ea75b1bd167a94d61d29cb2c47b1080f1

Make n-queens benchmark compatible with older GHCs
The change in 957b959dbeb14c3b2d10998d67310e880c01a7dd
caused n-queens to break with older versions of `base`
which still have `Foreign.unsafePerformIO` in place.

shootout/n-body/Main.hs
@@ -11,13 +11,13 @@
 -- Don't enable -optc-mfpmath=sse -optc-msse2, this triggers a gcc bug on x86
 --
 
-import Foreign
+import Foreign (Ptr, Storable(..), plusPtr, mallocBytes)
 import Foreign.Storable
 import Foreign.Marshal.Alloc
 import Data.IORef
 import Control.Monad
 import System.Environment
-import System.IO.Unsafe
+import System.IO.Unsafe (unsafePerformIO)
 import Text.Printf
 
 main = do